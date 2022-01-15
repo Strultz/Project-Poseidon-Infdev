877f8ae67839fd307413449b79304a7b6403b03c
Strultz <strultzgaming@gmail.com>
2022-01-14 21:51:09 -0600

    remove

--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn install:install-file -Dfile='./lib/bukkit-resources-0.0.1-SNAPSHOT.jar' -DgroupId='org.bukkit' -DartifactId='bukkit-resources' -Dversion='0.0.1-SNAPSHOT' -Dpackaging=jar -DlocalRepositoryPath='./lib'
